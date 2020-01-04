5965d64f9a08dc9e44dba59bf162890a17e552e0
lequanglam <lqlam2004@gmail.com>
2020-01-04 16:25:21 +0700

    Update

---
+++ index.js
@@ -0,0 +1,14 @@
+!global.data.werewolf ? global.data.werewolf = {} : "";
+
+function cmdinterface(type, data) {
+
+}
+
+function cmdconfig(type, data) {
+
+}
+
+exports = {
+    cmdinterface: cmdinterface,
+    cmdconfig: cmdconfig
+}
